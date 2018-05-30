57a739b1fe2fed72b7960e63b44940363a05914e
NTripoul <nicolas.tripoul+github@gmail.com>
2018-05-30 02:55:02 -0700

    Add script to convert data from edge list to totem and crunch vertex data files

---
+++ src/scripts/patternmatching/convert_edges2totem.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os, sys, subprocess
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+import argparse 
+
+    
+def main(inputFileName, outputFileName, directed=False):
+    lastline = str(subprocess.check_output(['tail', '-1', inputFileName]).decode('UTF-8'))
+    nodeNumberStr=str(int(lastline.split(' ')[0])+1)
+    edgesNumberStr = str(subprocess.check_output(['wc', '-l', inputFileName]).decode('UTF-8')).split(' ')[0]
+    fileSize = int(subprocess.check_output(['stat', '--printf="%s"', inputFileName]).decode('UTF-8').replace('"',''))
+    chunkSize=4096
+    with open(inputFileName) as inputFile, open(outputFileName,'w') as outputFile:
+        outputFile.write('#Nodes: '+nodeNumberStr+'\n')
+        outputFile.write('#Edges: '+edgesNumberStr+'\n')
+        if directed:
+            outputFile.write('#Directed'+'\n')
+        else:
+            outputFile.write('#Undirected'+'\n')
+                             
+        i=0
+        for chunk in iter(lambda: inputFile.read(chunkSize), ''):
+            if(i % 1000):
+                print('Current position : %d / %d'%(i*chunkSize,fileSize))
+            outputFile.write(chunk)
+            i+=1
+    inputFile.close()
+    outputFile.close()
+    print('Converted file ' + inputFileName + ' into '+outputFileName)
+    
+main('data/patternmatching/IMDB/patterns/test1/pattern_edge', 'data/patternmatching/IMDB/patterns/test1/pattern_edge.totem')
+#main('data/patternmatching/IMDB/all.sorted.edge', 'data/patternmatching/IMDB/graph.totem')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='convert_edges2totem.py', description='Generate constraint for pattern matching.')
+    parser.add_argument('-i', '--input', help='Input edges', required=True)
+    parser.add_argument('-o', '--output', help='Output file', required=True)
+    parser.add_argument('-d', '--directed', help='Directed graph',action='store_true')
+    try :
+        args=parser.parse_args()
+        
+        inputEdges=args.input
+        outputFile=args.output
+        directed=args.directed
+            
+        main(inputEdges, outputFile, directed)
+    except SystemExit:
+        pass
---
+++ src/scripts/patternmatching/crunch_vertex_data.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os, sys, subprocess
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+import argparse 
+
+    
+def main(inputFileList, outputFileName):
+    firstIdList=[]
+    for inputFileName in inputFileList:
+        firstId = int(subprocess.check_output(['head', '-n', '1', inputFileName]).decode('UTF-8').split(' ')[0])
+        firstIdList.append(firstId)
+        
+    print(firstIdList)
+            
+    chunkSize=4096
+    with open(outputFileName,'w') as outputFile:
+        for inputFileName in inputFileList:
+            with open(inputFileName) as inputFile:                             
+                print('Processing file : ' + inputFileName)
+                for chunk in iter(lambda: inputFile.read(chunkSize), ''):
+                    outputFile.write(chunk)
+            inputFile.close()
+    outputFile.close()
+    print('Converted files into '+outputFileName)
+
+inputDirectory='data/patternmatching/IMDB/vertex_data/test1/'
+main([inputDirectory+'vertex_data.actresses.list.vertex_data',\
+      inputDirectory+'vertex_data.actors.list.vertex_data',\
+      inputDirectory+'vertex_data.directors.list.vertex_data',\
+      inputDirectory+'vertex_data.movie.list.vertex_data',\
+      inputDirectory+'vertex_data.genres.list.vertex_data'],\
+    inputDirectory+'graph_vertex_data.totem')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='crunch_vertex_data.py', description='Generate constraint for pattern matching.')
+    parser.add_argument('-i', '--input_list', help='Input edges', nargs='+', required=True)
+    parser.add_argument('-o', '--output', help='Output file', required=True)
+    try :
+        args=parser.parse_args()
+        
+        inputEdges=args.input_list
+        outputFile=args.output
+            
+        main(inputEdges, outputFile, directed)
+    except SystemExit:
+        pass
